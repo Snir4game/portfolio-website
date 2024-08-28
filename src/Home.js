0ad8e4c1a7cf806bf2073535d22ba7884922af34
Snir4game <sniramsalem1995@gmail.com>
2024-08-28 23:41:03 +0300

    Components

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <section id="home">
+      <h1>Hello, I'm [Your Name]</h1>
+      <p>Welcome to my portfolio!</p>
+    </section>
+  );
+};
+
+export default Home;
---
+++ src/Projects.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+const Projects = () =>{
+    return(
+        <section id="projects">
+            <h2>Projects</h2>
+            <div>
+                <h3>GameHub Project</h3>
+                <p>GameHub its an Wikipedia Games Information </p>
+            </div>
+
+
+        </section>
+    )
+}
